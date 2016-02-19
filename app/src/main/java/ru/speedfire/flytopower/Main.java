e3ba2865e17a6ff9d6b12cfa8d258940e4bfc9c6
SpeedFire0 <a.kurapov@gmail.com>
2016-02-19 13:52:03 +0300

    Added 3rd option

--- app/src/main/java/ru/speedfire/flytopower/Main.java
+++ app/src/main/java/ru/speedfire/flytopower/Main.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -23,23 +24,29 @@ public class Main implements IXposedHookLoadPackage {
 
         Log.d(LOG_TAG, "We are now inside the original app - FlyAudio Media >>>>>>>>>>>>>>>>>>>>>>");
 
+        // =========================================================================================
+        // ===== OPTION 1: Hook before/after the original method (e.g. "onCreate") is executed =====
         // ============================================================================
-        // ===== OPTION 1: Hook before/after method (e.g. "onCreate") is executed =====
-        // ============================================================================
-/*        findAndHookMethod(XposedHelpers.findClass("cn.flyaudio.media.view.activity.MusicPlaybackActivity", lpparam.classLoader), "onCreate", Bundle.class, new XC_MethodHook() {
+        findAndHookMethod(XposedHelpers.findClass("cn.flyaudio.media.view.activity.MusicPlaybackActivity", lpparam.classLoader), "onCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 Log.d(LOG_TAG, "We are inside onCreate method");
+
 //                Context context = (Context) param.thisObject;
                 Activity activity = (Activity) param.thisObject;
 
-                  //This is to launch Google Music:
-//                Log.d(LOG_TAG, "Send Intent to launch Google Music");
-//                Intent intent = new Intent();
-//                intent.setAction("com.google.android.music.PLAYBACK_VIEWER");
-//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                context.startActivity(intent);
+                //This is to launch Google Music:
+ /*               Log.d(LOG_TAG, "Send Intent to launch Google Music");
+                Intent intent = new Intent();
+                intent.setAction("com.google.android.music.PLAYBACK_VIEWER");
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                activity.startActivity(intent);
 
+                Log.d(LOG_TAG, "Send Intent to start playing the track in Google Music");
+                Intent playIntent = new Intent("com.android.music.musicservicecommand");
+                playIntent.putExtra("command", "play");
+                activity.sendBroadcast(playIntent);
+*/
                 //PowerAmp
                 Log.d(LOG_TAG, "Send Intent to start PowerAmp service with Resume (play) function");
                 Intent pwramp_intent = new Intent("com.maxmpz.audioplayer.API_COMMAND");
@@ -53,11 +60,10 @@ public class Main implements IXposedHookLoadPackage {
                 pwramp_start_intent.setClassName("com.maxmpz.audioplayer", "com.maxmpz.audioplayer.StartupActivity");
                 activity.startActivity(pwramp_start_intent);
 
-
                 Log.d(LOG_TAG, "Intent is successfully sent !!!!");
+
                 Log.d(LOG_TAG, "Try to close the original app (FlyAudio player) - prevent from running >>>>>>");
                 activity.finish();
-
                 // Instead of activity.finish() you can use param.setResult(null) to prevent original app from running, but in my case the app was closed with error.
 //                param.setResult(null);
             }
@@ -68,11 +74,11 @@ public class Main implements IXposedHookLoadPackage {
             }
         });
 
-*/
+
         // ========================================================================
         // ===== OPTION 2: REPLACE method (e.g. "onCreate") with our own code =====
         // ========================================================================
-        findAndHookMethod(XposedHelpers.findClass("cn.flyaudio.media.view.activity.MusicPlaybackActivity", lpparam.classLoader), "onCreate", Bundle.class, new XC_MethodReplacement() {
+/*         findAndHookMethod(XposedHelpers.findClass("cn.flyaudio.media.view.activity.MusicPlaybackActivity", lpparam.classLoader), "onCreate", Bundle.class, new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 
@@ -92,6 +98,27 @@ public class Main implements IXposedHookLoadPackage {
 
                 Log.d(LOG_TAG, "Intent is successfully sent !!!!");
                 Log.d(LOG_TAG, "Try to close the original app (FlyAudio player) - prevent from running >>>>>>");
+                activity.finish();
+                return null;
+            }
+        });
+*/
+
+        // =================================================================================
+        // ===== OPTION 3: Option 1 or 2 + running new app (PowerAmp) from OUR SERVICE =====
+        // =================================================================================
+         findAndHookMethod(XposedHelpers.findClass("cn.flyaudio.media.view.activity.MusicPlaybackActivity", lpparam.classLoader), "onCreate", Bundle.class, new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+
+                Activity activity = (Activity) param.thisObject;
+                //Launching our MyService which will further send all intents independently
+
+                Log.d(LOG_TAG, "Send Intent to MyService");
+                Intent myservice_intent = new Intent("ru.speedfire.flytopower.MyService");
+                activity.startService(myservice_intent);
+
+                //Closing initial app:
                 activity.finish();
                 return null;
             }
